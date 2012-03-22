cdbc055fa21acfb55f2b39e8aec920f6793b1925
Blue.eyed.Penguin <Blue.eyed.Penguin@gmail.com>
2012-03-22 18:50:09 +0000

    can use %~group% to insert fields only from specific group. Added C++ serialization to and from XML. Added generator base class.

---
+++ Metamorphosis/Generators/Generator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metamorphosis
+{
+    class Generator
+    {
+        public static Generator Current;
+
+        public static void SetGenerator(string language)
+        {
+            switch (language.ToUpper())
+            {
+                case "C++":
+                    {
+                        Current = new CppGenerator();
+                        break;
+                    }
+
+                case "C#":
+                    {
+                        Current = new CsGenerator();
+                        break;
+                    }
+            }
+        }
+
+        public virtual void Generate()
+        {
+        }
+
+        public virtual void SetOutputFile(string outputFile)
+        {
+        }
+
+        public virtual string GetVirtualModificator(bool fromBase, bool toChild)
+        {
+            return "";
+        }
+
+        public virtual string GetNamespace(string metaNamespace)
+        {
+            return "";
+        }
+
+    }
+}
